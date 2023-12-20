b7c5ba3eabfa694640d59b727e6572a714e9df34
Mario Roberto <mrgomez.dev@gmail.com>
2023-12-20 10:09:43 -0600

    Add project web-server in this project I learned how to make request requests to a webservice by obtaining data through api

---
+++ web-server/main.py
@@ -0,0 +1,12 @@
+# Importar el módulo store
+import store
+
+# Definir la función run
+def run():
+
+   # Llamar a la función get_categories del módulo store
+   store.get_categories()
+
+    
+if __name__=='__main__':
+    run()
---
+++ web-server/store.py
@@ -0,0 +1,25 @@
+import requests
+
+# Definir la función get_categories
+def get_categories():
+
+   # Realizar una solicitud GET a la URL proporcionada y guardar la respuesta en la variable r
+   r = requests.get('https://api.escuelajs.co/api/v1/categories')
+
+   # Imprimir el código de estado HTTP de la respuesta
+   print(r.status_code)
+
+   # Imprimir el cuerpo de la respuesta como una cadena de texto
+   print(r.text)
+
+   # Imprimir el tipo de datos del cuerpo de la respuesta
+   print(type(r.text))
+
+   # Convertir el cuerpo de la respuesta en un objeto JSON y guardarlo en la variable category
+   category = r.json()
+
+   # Iterar sobre cada elemento en el objeto JSON category
+   for category in category:
+
+       # Imprimir el valor del campo 'name' de cada categoría en el objeto JSON
+       print(category['name'])
